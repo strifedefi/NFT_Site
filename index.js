e15f77b6141439b1c0c2c50a14d55c208d849e44
Peter Sarll <strifedefi@protonmail.com>
2022-06-01 20:22:30 -0700

    remove the javascript for now and create an else if in html that will detect metamask wallet

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import detectEthereumProvider from "./@metamask/detect-provider";
+// import detectEthereumProvider from "./@metamask/detect-provider";
 
-const provider = await detectEthereumProvider();
+// const provider = await detectEthereumProvider();
 
-if (provider) {
-  // From now on, this should always be true:
-  // provider === window.ethereum
-  startApp(provider); // initialize your app
-} else {
-  console.log("Please install MetaMask!");
-}
+// if (provider) {
+//   // From now on, this should always be true:
+//   // provider === window.ethereum
+//   startApp(provider); // initialize your app
+// } else {
+//   console.log("Please install MetaMask!");
+// }
 
-const ethereumButton = document.querySelector(".enableEthereumButton");
+// const ethereumButton = document.querySelector(".enableEthereumButton");
 
-ethereumButton.addEventListener("click", () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: "eth_requestAccounts" });
-});
+// ethereumButton.addEventListener("click", () => {
+//   //Will Start the metamask extension
+//   ethereum.request({ method: "eth_requestAccounts" });
+// });
